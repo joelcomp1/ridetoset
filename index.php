8ec2d5349c429cbfb18eb973bf77c7e162550a2a
unknown <joelcomp1@gmail.com>
2013-07-26 15:15:57 -0400

    e

--- index.php
+++ index.php
@@ -213,9 +213,9 @@ $app_name = idx($app_info, 'name', '');
       }(document, 'script', 'facebook-jssdk'));
 	  
 
-	$(document).ready(function() {
+$(document).ready(function() {
 	
-			var date = new Date();
+		var date = new Date();
 		var d = date.getDate();
 		var m = date.getMonth();
 		var y = date.getFullYear();
@@ -227,7 +227,51 @@ $app_name = idx($app_info, 'name', '');
 				right: 'month,agendaWeek,agendaDay'
 			},
 			editable: true,
-
+			events: [
+				{
+					title: 'All Day Event',
+					start: new Date(y, m, 1)
+				},
+				{
+					title: 'Long Event',
+					start: new Date(y, m, d-5),
+					end: new Date(y, m, d-2)
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: new Date(y, m, d-3, 16, 0),
+					allDay: false
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: new Date(y, m, d+4, 16, 0),
+					allDay: false
+				},
+				{
+					title: 'Meeting',
+					start: new Date(y, m, d, 10, 30),
+					allDay: false
+				},
+				{
+					title: 'Lunch',
+					start: new Date(y, m, d, 12, 0),
+					end: new Date(y, m, d, 14, 0),
+					allDay: false
+				},
+				{
+					title: 'Birthday Party',
+					start: new Date(y, m, d+1, 19, 0),
+					end: new Date(y, m, d+1, 22, 30),
+					allDay: false
+				},
+				{
+					title: 'Click for Google',
+					start: new Date(y, m, 28),
+					end: new Date(y, m, 29),
+					url: 'http://google.com/'
+				}
 			]
 		});
 		
@@ -391,7 +435,6 @@ $app_name = idx($app_info, 'name', '');
         </ul>
       </div>
     </section>
-	<div id='loading' style='display:none'>loading...</div>
 	<div id='calendar'></div>
     <?php
       }
