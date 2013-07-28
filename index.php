db42547267079b72f4b60f8488d78c05062398a7
unknown <joelcomp1@gmail.com>
2013-07-28 16:29:20 -0400

    e

--- index.php
+++ index.php
@@ -269,7 +269,53 @@ $(document).ready(function() {
 		});
 		$('#calendar').fullCalendar('getDate').addClass("fc-state-highlight");
 	  
+	$.fn.popOpen = function(){
+		
+		popID = $(this).attr('rel'); //Get Popup Name
+		popURL = $(this).attr('href'); //Get Popup href to define size
+		
+		//Pull Query & Variables from href URL
+		query= popURL.split('?');
+		dim= query[1].split('&');
+		popWidth = dim[0].split('=')[1]; //Gets the first query string value
+		
+		
+		//Fade in the Popup and add close button
+		$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend();
+		
+		//Define margin for center alignment (vertical + horizontal) - we add 80 to the height/width to accomodate for the padding + border width defined in the css
+		var popMargTop = ($('#' + popID).height() + 80) / 2;
+		var popMargLeft = ($('#' + popID).width() + 80) / 2;
+		
+		//Apply Margin to Popup
+		$('#' + popID).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+		
+		//Fade in Background
+		$('body').append('<div id="fade"></div>'); //Add the fade layer to bottom of the body tag.
+		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); //Fade in the fade layer 
+		
+	};
 
+	//When you click on a link with class of poplight and the href starts with a # 
+	$('a.poplight[href^=#]').click(function() {
+		popUp(); //Run popOpen function on click
+		return false;
+	});
+	
+	$('a.poplight[href=#?w=350]').popOpen(); //Run popOpen function once on load
+	
+	//Close Popups and Fade Layer
+	$('#fade').click( function() { //When clicking on the close or fade layer...
+	  	$('#fade , .popup_block').fadeOut(); //fade them both out
+		$('#fade').remove();
+		return false;
+	});
+
+
+	popOpen
 	});
     </script>
 <style>
@@ -320,10 +366,27 @@ value="Start Typing Shows here..." onfocus="this.value = this.value=='Start Typi
 		&nbsp;
 	</div>
 	</div>
+	<a href="#" onclick="popup(350, 'popup4');" class="poplight">Add Show?</a>
+	<div id="popup4" class="popup_block">
+
+	
+	<h2 style=" padding:0px 0 0 30px!important; float:none; text-align: left;">Reset Password</h2>
+	<p id="contactArea" style=" padding:0px 0 0 30px!important; float:none; text-align: left;">
+	Enter the email address you used whne creating your account.  Hit the send button and we will send you an email with instructions to reset your password.
+	<br><br>
+	<form id="loginForm" name="loginForm" method="post" action="php/reset-password.php">
+	<b>Email</b>
+	  <input name="email" type="text" class="textfield" id="email" tabindex="1" autofocus style="color:#0000;" value="E-mail" onfocus="this.value = this.value=='E-mail' ? '' : this.value; this.style.color='#000';" onfocusout="this.value = this.value == '' ? this.value = 'E-mail' : this.value; this.value=='E-mail' ? this.style.color='#ccc' : this.style.color='#000'" />
+	&nbsp;<input type="submit" name="Submit" value="Send"  tabindex="1"/>
+
+	</form>
+	</p>
+	 
+	</div>
 </div>
 <br><br>
-      <div class="list" style="text-align:center; float:left;">
-        <h1>Friends using this app</h1>
+      <div class="list" style="text-align:center;">
+        <h1 style="float:left;">Friends using this app</h1>
        <div id="share-app">
           <!--p>Share your app:</p-->
           <ul>
