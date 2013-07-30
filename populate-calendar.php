5b498de78611015e3c54b5dbecc69b1b7f5e795a
unknown <joelcomp1@gmail.com>
2013-07-29 20:59:33 -0400

    updates

--- populate-calendar.php
+++ populate-calendar.php
@@ -51,11 +51,11 @@
 			$dateConvert =  date('H:i:s', strtotime($row['call_time']))
 			//$datetime->add(new DateInterval($dateConvert));
 			$totalPrograms[$index]['id'] = $index;
-			$totalPrograms[$index]['title'] = $time;
+			$totalPrograms[$index]['title'] = $row['show_name'];
 			$totalPrograms[$index]['start'] = $dateConvert;
 		//	$totalPrograms[$index]['end'] = DateTime('2013-7-28 23:59:59');
 			$totalPrograms[$index]['url'] = "index.php?showname=";
-			$totalPrograms[$index]['url'] .= $row['show_name'];
+			$totalPrograms[$index]['url'] .= $dateConvert;
 
 			$index += 1;
 	}
