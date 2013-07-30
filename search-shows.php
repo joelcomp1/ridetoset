2cea1ffbde3b9a440a905aafa7c4da396a25208e
unknown <joelcomp1@gmail.com>
2013-07-30 18:27:05 -0400

    updates

--- search-shows.php
+++ search-shows.php
@@ -4,14 +4,14 @@
 	
 	//Include database connection details
 	include("AppInfo.php");
-	
+	include('utils.php');
 	//Array to store validation errors
 	$errmsg_arr = array();
 	
 	//Validation error flag
 	$errflag = false;
-	
-	
+	 
+	$facebook = $_SESSION['facebook']; 
 
 		// Is there a posted query string?
 		if(isset($_POST['queryString'])) {
@@ -30,18 +30,28 @@
 				// YOU NEED TO ALTER THE QUERY TO MATCH YOUR DATABASE.
 				// eg: SELECT yourColumnName FROM yourTable WHERE yourColumnName LIKE '$queryString%' LIMIT 10
 				
-				$query = mysql_query("SELECT show_name FROM shows WHERE show_name LIKE '$queryString%' LIMIT 25");
+				$query = mysql_query("SELECT show_name, user_id FROM shows WHERE show_name LIKE '$queryString%' LIMIT 25");
 
 				if($query) {
 			
 					// While there are results loop through them - fetching an Object (i like PHP5 btw!).
 					while ($result = mysql_fetch_object($query)) {
 	
-						// Format the results, im using <li> for the list, you can change it.
-						// The onClick function fills the textbox with the result.
-						$_SESSION['showName'] = $result->show_name;
-						// YOU MUST CHANGE: $result->value to $result->your_colum
-	         			echo '<li onClick="fillTags(\''.$result->show_name.'\');">'.$result->show_name.'</li>';
+					foreach($facebook as $value)
+					{
+						if(idx($value, 'username') == $result['user_id'] ||
+						$result['user_id'] == clean($_SESSION['user_id']))
+						{
+							// Format the results, im using <li> for the list, you can change it.
+							// The onClick function fills the textbox with the result.
+							$_SESSION['showName'] = $result->show_name;
+							// YOU MUST CHANGE: $result->value to $result->your_colum
+							echo '<li onClick="fillTags(\''.$result->show_name.'\');">'.$result->show_name.'</li>';
+		
+						}
+					}
+	
+
 	         		}
 				} else {
 					echo 'ERROR: There was a problem with the query.';
