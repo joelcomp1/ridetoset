68ac59712f1b10b609487405acff0b0460c6a510
unknown <joelcomp1@gmail.com>
2013-07-30 18:38:22 -0400

    updates

--- search-shows.php
+++ search-shows.php
@@ -7,7 +7,13 @@
 	include('utils.php');
 	//Array to store validation errors
 	$errmsg_arr = array();
-	
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return  mysql_escape_string($str);
+	}
 	//Validation error flag
 	$errflag = false;
 	$add_event = false;
@@ -37,7 +43,7 @@
 					// While there are results loop through them - fetching an Object (i like PHP5 btw!).
 					while ($result = mysql_fetch_object($query)) {
 	
-					/*foreach($facebook as $value)
+					foreach($facebook as $value)
 					{
 						if(idx($value, 'username') == $result->user_id ||
 						$result->user_id == clean($_SESSION['user_id']))
@@ -52,13 +58,13 @@
 		
 					}
 					if($add_event == true)
-					{*/
+					{
 							// Format the results, im using <li> for the list, you can change it.
 							// The onClick function fills the textbox with the result.
 							$_SESSION['showName'] = $result->show_name;
 							// YOU MUST CHANGE: $result->value to $result->your_colum
 							echo '<li onClick="fillTags(\''.$result->user_id.'\');">'.$result->user_id.'</li>';
-					//}
+					}
 						
 					}
 
