271346bbe5c664ebe329cf6e1855fee13464615e
Joel Millage <joelcomp1@gmail.com>
2013-08-10 09:45:37 -0400

    update

--- populate-calendar.php
+++ populate-calendar.php
@@ -59,13 +59,15 @@ function clean($str) {
 				{
 					$firstTime = date_format($datetime->format(DateTime::ISO8601), 'H:i:s');
 					$secondTime = date_format($show['start'],  'H:i:s');
+					$add_event = false;
 					if(firstTime <= $secondTime)
 					{
-						$add_event = false;
+						
+						$show['start'] = $datetime->format(DateTime::ISO8601);
 					}
 					else
 					{
-						//$show['start'] = $datetime->format(DateTime::ISO8601);
+						
 					}					
 				}
 			}
