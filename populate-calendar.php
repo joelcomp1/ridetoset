237250b06bb42ccad2c0f0889a582a72b8e1e833
Joel Millage <joelcomp1@gmail.com>
2013-08-26 18:17:26 -0400

    update

--- populate-calendar.php
+++ populate-calendar.php
@@ -6,7 +6,7 @@
 	include('utils.php');
 require_once('PhpConsole.php');
 PhpConsole::start(true, true, dirname(__FILE__));
-debug('debug message');
+
 
 
 
@@ -62,6 +62,7 @@ function clean($str) {
 				/*see if the existing show is later or eariler*/
 				if($firstDate == $secondDate)
 				{
+					debug($firstDate);
 					$firstTime = date_format($datetime->format(DateTime::ISO8601), 'H:i:s');
 					$secondTime = date_format($show['start'],  'H:i:s');
 					//$add_event = false;
