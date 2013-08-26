7f3f977fb7b4b39c312ab6fb15f98b3d5b490ae7
Joel Millage <joelcomp1@gmail.com>
2013-08-26 19:09:54 -0400

    update

--- whos-going.php
+++ whos-going.php
@@ -23,11 +23,12 @@ PhpConsole::start(true, true, dirname(__FILE__));
 	$add_event = false;
 	$facebook = $_SESSION['facebook']; 
 	$dateFromCal = clean($_GET['date']);
-    $dt = new DateTime($dateFromCal);
- //   $dt->setTimestamp($dateFromCal);
+    $dt = new DateTime();
+	$dt->setTimestamp(strtotime($dateFromCal));
 
         //just for the fun: what would it be in UTC?
      $would_be = $dt->format('D M d Y');
+	 debug(strtotime($dateFromCal));
 	debug($would_be);
 	debug($dateFromCal);
 		    $show_name = clean($_SESSION['show_name']);
