648b852c770d2f3cf69b24d008caadb2ae4a8f23
Joel Millage <joelcomp1@gmail.com>
2013-08-27 16:24:04 -0400

    update

--- whos-going.php
+++ whos-going.php
@@ -76,7 +76,7 @@ PhpConsole::start(true, true, dirname(__FILE__));
 					<?php
 					
 							echo '<div id="imagegoing"><p id="picture" style="background-image: url(https://graph.facebook.com/'. idx($value, 'uid') . '/picture?type=normal); width:64px; height:64px; margin-right: 20px; float:left; background-position: center 25%;background-repeat: no-repeat;background-size: 64px;"></p>';
-							echo  '<h2><strong>'.idx($value, 'name').'</strong></h2>';
+							echo  '<h2 style="width: 300px;"><strong>'.idx($value, 'name').'</strong></h2>';
 							echo '<input type="button" id="sendMessage" value="Send Message" /></div></div>';
 							break;
 						}
