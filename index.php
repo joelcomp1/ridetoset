7736b156b84c4f18c6035524a95f4ded7f4a2883
Joel Millage <joelcomp1@gmail.com>
2013-08-27 18:00:40 -0400

    update

--- index.php
+++ index.php
@@ -394,7 +394,7 @@ $(document).ready(function() {
 				var hour = d.getMonth();
 				document.getElementById("header_show_time").innerHTML = formatAMPM(d);
 				s = s.substring(0, s.indexOf(d.getHours()));
-				s = s.substring(0, s.indexOf(" 0 "));
+				s = s.substring(0, s.indexOf(" 0"));
 				document.getElementById("header_show_time").innerHTML = s;
 				document.getElementById("header_show_time").innerHTML += " at ";
 				document.getElementById("header_show_time").innerHTML += formatAMPM(d);
