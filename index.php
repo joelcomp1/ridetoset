d83699dd41d9c4abe259ef6a64790f0836ca3d05
Joel Millage <joelcomp1@gmail.com>
2013-08-26 20:09:45 -0400

    update

--- index.php
+++ index.php
@@ -332,10 +332,8 @@ $(document).ready(function() {
 			  $('#timewhosgoing').hide();
 			  var s = new String (calEvent.start);
 			  s = s.substring(0, s.indexOf('GMT'));
-				$('#daycalendar')
-                .fullCalendar('changeView', 'agendaDay'/* or 'basicDay' */)
-                .fullCalendar('gotoDate',
-                    2013, 8, 6);
+				$('#daycalendar').fullCalendar('gotoDate',
+                    2013, 7, 6);
 					
 				document.getElementById("header_show_name").innerHTML = calEvent.title;
 				 jQuery.ajax({
