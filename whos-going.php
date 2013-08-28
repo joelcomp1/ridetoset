3481ada82584c1f74fc9d911516c6665aadd08d4
Joel Millage <joelcomp1@gmail.com>
2013-08-27 20:22:37 -0400

    update

--- whos-going.php
+++ whos-going.php
@@ -42,13 +42,13 @@ PhpConsole::start(true, true, dirname(__FILE__));
     <meta charset="utf-8" />
 	</head>
 	</body>
-	 <div style="font-size: 22px;">
+	 <div style="font-size: 20px;">
 	<?php
 	     echo 'Location: '.$result->city . ', ' . $result->state;          
 	?>
 	</div><br>
 	<label style="font-size: 26px;"><b>Who's Going:</b></label>
-	<div style="clear:both; font-weight: bold; text-align:center; width: 300px; margin-left: 120px;">
+	<div style="clear:both; font-weight: bold; text-align:center; width: 300px; margin-left: 140px;">
 	
 	<?php
 		
