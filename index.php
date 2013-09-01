b6fa6ec4ee5dbefae4c9428102f75e200dbbe983
Joel Millage <joelcomp1@gmail.com>
2013-09-01 15:09:19 -0400

    update

--- index.php
+++ index.php
@@ -400,13 +400,6 @@ $(document).ready(function() {
         // your event source
         {
             url: '/populate-calendar-day.php', // use the `url` property
-        },
-		
-		{
-            events: function(start, end, callback) {
-              document.getElementById("hiddendate").innerHTML = start;
-				$("[name='hiddendate']").val(start);
-            }
         }
 
 		],
