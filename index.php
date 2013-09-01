861a79660cf92e35422568f8fe980072ac1082ce
Joel Millage <joelcomp1@gmail.com>
2013-09-01 15:03:37 -0400

    update

--- index.php
+++ index.php
@@ -400,16 +400,16 @@ $(document).ready(function() {
         // your event source
         {
             url: '/populate-calendar-day.php', // use the `url` property
-        }],
-		events: function(start, end, callback) {
-		$.ajax({
-            url: 'populate-calendar-day.php'
-        });
+        }
 		
-		
-				document.getElementById("hiddendate").innerHTML = start;
+		{
+            events: function(start, end, callback) {
+              document.getElementById("hiddendate").innerHTML = start;
 				$("[name='hiddendate']").val(start);
-		},
+            }
+        }
+
+		],
 			//events: "populate-calendar-day.php",
 			eventClick: function(calEvent, jsEvent, view) {
 				 $('#daycalendar').hide();
