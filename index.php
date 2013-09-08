55e6671f8fe087a86215ccec8524c7426b4f3c61
Joel Millage <joelcomp1@gmail.com>
2013-09-08 18:12:44 -0400

    update

--- index.php
+++ index.php
@@ -333,7 +333,45 @@ $(document).ready(function() {
 		var d = date.getDate();
 		var m = date.getMonth();
 		var y = date.getFullYear();
-			
+		var id = '#dialog';
+	
+		//Get the screen height and width
+		var maskHeight = $(document).height();
+		var maskWidth = $(window).width();
+	
+		//Set heigth and width to mask to fill up the whole screen
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+		
+		//transition effect		
+		$('#mask').fadeIn(1000);	
+		$('#mask').fadeTo("slow",0.8);	
+	
+		//Get the window height and width
+		var winH = $(window).height();
+		var winW = $(window).width();
+              
+		//Set the popup window to center
+		$(id).css('top',  winH/2-$(id).height()/2);
+		$(id).css('left', winW/2-$(id).width()/2);
+	
+		//transition effect
+		$(id).fadeIn(2000); 	
+	
+	//if close button is clicked
+	$('.window .close').click(function (e) {
+		//Cancel the link behavior
+		e.preventDefault();
+		
+		$('#mask').hide();
+		$('.window').hide();
+	});		
+	
+	//if mask is clicked
+	$('#mask').click(function () {
+		$(this).hide();
+		$('.window').hide();
+	});		
+	
 		$('#calendar').fullCalendar({
 			header: {
 				left: 'prev,next today',
@@ -497,6 +535,31 @@ $(document).ready(function() {
    display : none;
 }
 
+#mask {
+  position:absolute;
+  left:0;
+  top:0;
+  z-index:9000;
+  background-color:#000;
+  display:none;
+}  
+#boxes .window {
+  position:absolute;
+  left:0;
+  top:0;
+  width:440px;
+  height:200px;
+  display:none;
+  z-index:9999;
+  padding:20px;
+}
+#boxes #dialog {
+  width:375px; 
+  height:203px;
+  padding:10px;
+  background-color:#ffffff;
+}
+
 </style>
     <header class="clearfix">
 	
@@ -567,6 +630,14 @@ value="Start Typing Shows here..." onfocus="this.value = this.value=='Start Typi
 
 	<!--a href="#" id="try-2" class="try sprited"><img src="../images/help.png" style="padding: 0px 0px 0px 20px;"></a-->
 
+</div>
+<div id="boxes">
+<div style="top: 199.5px; left: 551.5px; display: none;" id="dialog" class="window">
+Simple Modal Window | 
+<a href="#" class="close">Close it</a>
+</div>
+<!-- Mask to cover the whole screen -->
+<div style="width: 1478px; height: 602px; display: none; opacity: 0.8;" id="mask"></div>
 </div>
 	<br>
     <div id='calendar'></div>
