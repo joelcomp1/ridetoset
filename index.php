c0b9b1f30308b6546535bf9e2df3e6ba20295a33
Joel Millage <joelcomp1@gmail.com>
2013-09-08 18:08:52 -0400

    update

--- index.php
+++ index.php
@@ -537,7 +537,7 @@ $(document).ready(function() {
 ?>
 <a href="http://forum.ridetoset.com" style="float:left;"><img src="images/forum_banner.png" height=150px width=300px style="margin-top: 50px;"></a>
 
-<div style="text-align:center; float:left; margin-top:0px; margin-right: 25px; margin-left: 25px;">
+<div style="text-align:center; float:left; margin-top:100px;">
 		<input type="button" id="try-1" class="try sprited" value="Add Show!">
 	</div>
 	
