43cfdb6fd5e9ce6f59678f234525e4baf4936798
Joel Millage <joelcomp1@gmail.com>
2013-09-14 16:02:02 -0400

    update

--- index.php
+++ index.php
@@ -213,16 +213,7 @@ $app_name = idx($app_info, 'name', '');
           xfbml      : true // parse XFBML
         });
 
-        // Listen to the auth.login which will be called when the user logs in
-        // using the Login button
-        FB.Event.subscribe('auth.login', function(response) {
-          // We want to reload the page now so PHP can read the cookie that the
-          // Javascript SDK sat. But we don't want to use
-          // window.location.reload() because if this is in a canvas there was a
-          // post made to this page and a reload will trigger a message to the
-          // user asking if they want to send data again.
-          window.location = window.location;
-        });
+   
 
 		
         FB.Canvas.setAutoGrow();
