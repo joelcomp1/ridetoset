45f5ccca615b29a82071a3281493a1a8761f3d5f
unknown <joelcomp1@gmail.com>
2013-10-26 17:02:43 -0400

    update

--- index.php
+++ index.php
@@ -669,8 +669,8 @@ value="Start Typing Shows here..." onfocus="this.value = this.value=='Start Typi
 </div>
 <div style="text-align:center;clear:both;width:728px">
 <input type="button" id="try-1" class="try sprited" value="Add Show!" style="width:200px; height: 60px;">
-<a href="http://forum.ridetoset.com" style="float:left;"><img src="images/forum_banner_mobile.png" width=200 height=60></a>
-<a href="" style="float:left;"><img src="images/tutvideobannermobile.png" width=200 height=60></a>
+<a href="http://forum.ridetoset.com" style=""><img src="images/forum_banner_mobile.png" width=200 height=60></a>
+<a href="" style=""><img src="images/tutvideobannermobile.png" width=200 height=60></a>
 </div>
 
 
