f0f0a73eeb68628040c0dda9f276c140b076c696
unknown <joelcomp1@gmail.com>
2014-02-22 10:47:10 -0500

    mobile web modifications

--- index.php
+++ index.php
@@ -59,8 +59,8 @@ if ($user_id) {
     // If the call fails we check if we still have a user. The user will be
     // cleared if the error is because of an invalid accesstoken
     if (!$facebook->getUser()) {
-      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
-      exit();
+     // header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+     // exit();
     }
   }
  
